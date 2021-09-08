8393a042e60c1f7413edd8b1aeaa0d0a2ce5ef0a
sophian_achiba <sophian_achiba@calypso.com>
2021-09-07 21:56:04 -0700

    queue
    Change-Id: I639c080641ed9af54cbe8c78e4de35d79128a842

--- src/api/tests/QueueAPITest.java
+++ src/api/tests/QueueAPITest.java
@@ -1,5 +1,6 @@
 package src.api.tests;
 import src.api.util.*;
+import java.util.*;
 
 public class QueueAPITest {
     
@@ -7,28 +8,22 @@ public class QueueAPITest {
 
         String yours = "";
         String expected = "";
-        // create a Qeueu
-
-        // add the following : 1,2 using 2 differen way
-
-        // print head of queue using 2 different ways
-        yours="";
-        expected = "1 1";
-
-        TestAPIUtil.assertValue(yours, expected);
-        // remove both element using 2 different way;
-
-        // print head 2 different ways
-        yours="";
-        expected = "null null";
-
-        TestAPIUtil.assertValue(yours, expected);
-
-        // CORE METHOD Test
-        // created a linkedlist, add: 1,2, print head and remove head, print new head (not removed).
-        yours = "";
-        expected = "1 2";
+        // create a Queue
         
+        // add the following : 1,2
+
+        // print head of queue but dont take out it
+        yours="";
+        expected = "1";
+
+        TestAPIUtil.assertValue(yours, expected);
+
+        // take all elements out of the queue
+
+        // print head
+        yours="";
+        expected = "null";
+
         TestAPIUtil.assertValue(yours, expected);
     }
 }
